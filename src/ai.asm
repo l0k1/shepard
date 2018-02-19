77bc63e0593a9ba62f5b6bc50eed1de637f53210
Justin Nicholson <justi.n.icholson@gmail.com>
2018-02-19 14:21:21 -0800

    More AI work.

--- src/ai.asm
+++ src/ai.asm
@@ -18,9 +18,26 @@ AI:
    jr nc,.no_load_parasite
 
 .load_parasite
-   ld A,[OAM_POINTER]
-   ld H,$DF
+   ; find the first unused sprite
+   ld HL,[OAM_MIRROR + $03]   ; skip the player sprite
+   ld E,$28
+   ; point HL at the tile
+   ; 40 possible sprites
+.find_unused_tile_parasite
+   dec E
+   ld A,E
+   or $00
+   jr z,.no_load_parasite
+   ld A,L
+   add $04
    ld L,A
+   ld A,[HL]
+   cp $00
+   jr nz,.find_unused_tile_parasite
+
+   ; point HL at the y attrib for that sprite
+   dec HL
+   dec HL
 
    call Get_Random
 
@@ -30,14 +47,11 @@ AI:
    xor A
    ld [HL+],A
    ; tile, parasite = 4
-   ld A,$04
+   ld A,$03
    ld [HL+],A
    ; attribs
    xor A
    ld [HL+],A
-
-   ld A,L
-   ld [OAM_POINTER],A
 
    ld HL,PARA_COUNT
    inc [HL]
@@ -47,7 +61,29 @@ AI:
 ; cow loading
 
 ; movement
+; do this one sprite at a time
+   ld E,$28
+   ld HL,[OAM_MIRROR + $03]
+.movement
+   dec E
+   ld A,E
+   or $00
+   jr nz,.ret              ; cancel when we reach the end of the sprites
+   ld A,L
+   add $04
+   ld L,A                  ; inc HL by 4 to check the next sprite
+   ld A,[HL]
+   cp $02
+   jr z,.cow_movement
+   cp $03
+   jr z,.parasite_movement
+   jr .movement            ; if not a parasite or cow, skip it
 
+.parasite_movement
+   push DE                 ; our main loop needs these two
+   push HL
 
+   
 
+.ret
    ret
