aa2cd6bf993d047fc591ab2a10092b233f2b2e2e
Justin Nicholson <justi.n.icholson@gmail.com>
2018-12-31 23:09:29 +0000

    remove exports from sprites.asm

--- src/sprites.asm
+++ src/sprites.asm
@@ -1,17 +1,12 @@
 ; sprites
-
-EXPORT Shepard
-EXPORT Cow
-EXPORT Parasite
-
  SECTION "Sprites",ROM0
 
-Shepard:
+Shepard::
 DB $00,$08,$18,$5A,$3C,$24,$66,$5A
 DB $7E,$C3,$3C,$24,$18,$5A,$00,$10
-Cow:
+Cow::
 DB $00,$24,$00,$42,$66,$66,$FF,$BD
 DB $FF,$E7,$3C,$24,$3C,$7E,$00,$42
-Parasite:
+Parasite::
 DB $00,$20,$00,$34,$08,$53,$38,$1E
 DB $1C,$78,$10,$CA,$00,$2C,$00,$04
