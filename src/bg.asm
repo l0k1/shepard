d0415afe0449899905bd0cc0e8b79927fc54aedf
Justin Nicholson <justi.n.icholson@gmail.com>
2019-01-09 15:44:39 -0800

    [2f9] more bg scrollbar work

--- src/bg.asm
+++ src/bg.asm
@@ -10,8 +10,34 @@ INCLUDE "defines.asm"
    SECTION "BG",ROM0
 Inc_BG::
    ; call this function to increment the background scroller
+   
+   ; get bg addr into HL
    push AF
+   push BC
+   push DE
    push HL
    ld A,[BG_ADDR_REF]
    ld H,$9A
    ld L,A
+   ld A,[HL]
+   
+   ; the actual tile will be at $9000 + (bg_addr_ref - $20) * $10 + 18 or 19
+   sub $20
+   ; multiply by $10
+   rlca
+   rlca
+   ; subtract by either 1 or 2 depending on the runthru
+   ld D,A
+   ld A,[BG_RUNTHRU]
+   cp $00
+   jr z,.add18
+   cp $02
+   jr z,.add18
+   add $19
+   jr .cont
+.add18
+   sub $18
+.cont
+   ; check for carry
+   
+   
